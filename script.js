aa2658846c35fc5decc595ed196aeb289763fad4
Jon0710 <jtmendoza2@student.apc.edu.ph>
2024-02-24 23:07:01 +0800

    week5 date and time

--- script.js
+++ script.js
@@ -21,3 +21,18 @@ if (availableForHire) {
 
 let greeting = "Hello, Welcome to my Personal Website!";
 console.log(greeting);
+
+
+var d = new Date();
+var b = d.getDate();            
+var c = d.getFullYear();       
+var e = d.getHours();           
+var g = d.getMinutes();        
+var h = d.getMonth() + 1;                   
+
+console.log("Month: " + h);
+console.log("Day of the month: " + b);
+console.log("Year: " + c);
+console.log("Hour: " + e);
+console.log("Minutes: " + g);
+
