6975931ac449d615ccc8dd1f0082bddc4f99dec1
Jon0710 <jtmendoza2@student.apc.edu.ph>
2024-02-24 22:44:49 +0800

    week5 variables

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("Welcome to my personal profile!");
+if (!sessionStorage.getItem('alertShown')) {     // If not, show the alert     
+    alert("Welcome to my personal profile!");     // Mark the alert as shown in sessionStorage     
+    sessionStorage.setItem('alertShown', 'true');   }
+
+let name = "Jon Morieshi";
+let age = 20;
+let occupation = "Student";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Occupation:", occupation);
